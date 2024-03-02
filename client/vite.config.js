1c2b4c53192770bbc7e197fd66916f0a50948012
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-03-02 20:46:56 +1100

    working on front end - login auth form

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 
+const BASE_URL = 'http://localhost:3000'
+
 export default defineConfig({
     resolve: {
         alias: {
             '@': "/src",
             '~@': "/src",
+        }
+    },
+    server: {
+        proxy: {
+            "/login": BASE_URL + "/views/login",
         }
     }
 })
