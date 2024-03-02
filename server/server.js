7a826bc6afe853c82b8cc1daaeabf8c0c939e89e
Bonsen Wakjira <bwak0002@student.monash.edu>
2024-03-02 13:59:22 +1100

    Removed mongodb and ejs layouts

--- server/server.js
+++ server/server.js
@@ -10,22 +10,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('view engine', 'ejs')
 app.set('views', `${__dirname}/views`);
-app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-// Database connection =================
-
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-dotenv.config();
-
-mongoose.connect(process.env.DATABASE_URL)
-
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log("Connected to Mongoose"))
 
 // Routes for the API ============================
 import root from './api/root.js'
